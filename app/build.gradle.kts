6c21cf5a5e7e7b76471edd65ebdcd340290267d9
aslarmy <cupplate8@gmail.com>
2025-11-20 15:09:33 +0600

    update ci and cd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,13 +24,8 @@ android {
         minSdk = 24
         targetSdk = 36
 
-        // Automatically set versionName from tag
-        val gitTag = findProperty("GIT_TAG") as? String ?: "0.0.0"
-        versionName = gitTag.removePrefix("v")
-
-        // Automatically set versionCode based on commits
-        val versionCodeFromGit = "git rev-list --count HEAD".runCommand()
-        versionCode = versionCodeFromGit.toInt()
+        versionCode = 1
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -98,7 +93,3 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
-
-// Helper function to run shell commands in Gradle
-fun String.runCommand(): String =
-    Runtime.getRuntime().exec(this).inputStream.bufferedReader().readText().trim()
